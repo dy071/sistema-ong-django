8f1d9419c14342a70d9ea9bfcc75ab37ac330139
dy071 <diana.ribeiro071@gmail.com>
2026-05-16 15:53:14 -0300

    ocultando a secret key

--- setup/settings.py
+++ setup/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#%ysrccv$0t7gvt_vw578gbn_x07)9=x^ow__sa7*cqnvs0&_k'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'chave-secreta')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
